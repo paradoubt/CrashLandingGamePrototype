56829271785e95e618cd84853de496a07b1f0eb4
paradoubt <mariana.lobeto@knights.ucf.edu>
2019-03-24 21:24:05 -0400

    Audio
    Audio and audio code working

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/BackSFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioClip MusicClip;
+
+    public AudioSource Musicsource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Musicsource.clip = MusicClip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            Musicsource.Play();
+    }
+}
